0efc43edc97e5ed47a62fa4e493a48745dc5c273
spjoes <34802651+spjoes@users.noreply.github.com>
2022-06-21 01:15:03 -0400

    fixed /nickname command

--- commands/nickname.js
+++ commands/nickname.js
@@ -4,33 +4,25 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('nickname')
         .setDescription("Sets user's nickname")
-        .addUserOption(option =>
-            option.setName('username')
-                .setDescription('User to set nickname for')
-                .setRequired(true))
         .addStringOption(option2 =>
             option2.setName('nickname')
                 .setDescription('Nickname to set to user')
-                .setRequired(true)),
+                .setRequired(true))
+        .addUserOption(option =>
+            option.setName('username')
+                .setDescription('User to set nickname for')
+                .setRequired(false)),
     async execute(interaction) { 
+        if(!interaction.member.permissions.has("ADMINISTRATOR")) return interaction.reply({content: "You do not have permission to use this command!", ephemeral: true});
         var nickname = interaction.options.getString("nickname")
-        var username = interaction.options.get("username")
-        var enabled = false
-
-        if(enabled = false) {
-            interaction.reply({ content: 'this command is currently out of order', ephemeral: true});
-        } else if (enabled = true) {
-            if(username == "") {
-                username = interaction.author.username
-                //message.guild.members.get(username).setNickname(nickname)
-                //set nickname
-                interaction.guild.members.get(username).setNickname(nickname)
-                interaction.reply({ content: 'set your nickname to ' + nickname, ephemeral: true});
-            } else {
-                username = interaction.options.getString("user")
-                interaction.guild.members.get(username).setNickname(nickname)
-                interaction.reply({ content: 'set' + username + "'s " +'nickname to ' + nickname});
-            }
+        var username;
+        if(interaction.options.getUser("username") === undefined || interaction.options.getUser("username") === null) {
+            username = interaction.user;
+        } else {
+            username = interaction.options.getUser("username");
         }
+        if (username.id === interaction.guild.ownerId) return interaction.reply({content: "I can't change the owner's nickname.", ephemeral: true});
+        interaction.guild.members.cache.get(username.id).setNickname(nickname)
+        interaction.reply({ content: `set ${username.username}'s nickname to ${nickname}`, ephemeral: true});
     }
 };
