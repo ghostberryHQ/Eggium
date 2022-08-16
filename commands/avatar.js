6a2fdf10e06afdb925c6e1d12f9d119b81982ed8
spjoes <34802651+spjoes@users.noreply.github.com>
2022-08-16 13:05:08 -0400

    userinfo and avatar commands nolonger require you to enter a user

--- commands/avatar.js
+++ commands/avatar.js
@@ -11,21 +11,29 @@ module.exports = {
         .addMentionableOption(option =>
             option.setName('username')
                 .setDescription('The user you want to get the avatar of')
-                .setRequired(true)),
+                .setRequired(false)),
     async execute(interaction) {
         // console.log(await interaction.channel.messages.fetch(interaction.targetId))
-        var username = interaction.options.get("username")
-        console.log(username.user.username)
+        var username;
+        var id;
+        if(interaction.options.get("username") === undefined || interaction.options.get("username") === null) {
+            username = interaction;
+            id = username.user.id;
+        } else {
+            username = interaction.options.get("username");
+            id = username.value;
+        }
+        setTimeout(function() {
+            console.log(username.user.username)
 
-        const embed = new EmbedBuilder()
-            .setTitle('Avatar - ' + username.user.username)
-            .setColor('#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'))
-            .setThumbnail('https://cdn.discordapp.com/avatars/'+username.value+'/'+username.user.avatar+'.png')
-            .setDescription('https://cdn.discordapp.com/avatars/'+username.value+'/'+username.user.avatar+'.png')
-            embed.setFooter({
-                text: "Eggium - Tanner Approved"
-            })
-            .setTimestamp();
-        interaction.reply({ embeds: [embed] });
+            const embed = new EmbedBuilder()
+                .setTitle('Avatar - ' + username.user.username)
+                .setColor('#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'))
+                .setThumbnail('https://cdn.discordapp.com/avatars/'+id+'/'+username.user.avatar+'.png')
+                .setDescription('https://cdn.discordapp.com/avatars/'+id+'/'+username.user.avatar+'.png')
+                .setFooter({ text: "Eggium - Tanner Approved" })
+                .setTimestamp();
+            interaction.reply({ embeds: [embed] });
+        }, 1000);
     }
 };
--- commands/userinfo.js
+++ commands/userinfo.js
@@ -11,41 +11,48 @@ module.exports = {
         .addMentionableOption(option =>
             option.setName('username')
                 .setDescription('The user you want to get the info of')
-                .setRequired(true)),
+                .setRequired(false)),
     async execute(interaction) {
         // console.log(await interaction.channel.messages.fetch(interaction.targetId))
-        var username = interaction.options.get("username")
-        console.log(username)
-
-        //get user's id
-        var id = username.user.id;
-        //get user's tag
-        var tag = username.user.tag;
-        //get user's nickname
-        var nickname;
-        if(username.member.nickname === null) {
-            nickname = "[not yet set]"
+        var username;
+        var id;
+        if(interaction.options.get("username") === undefined || interaction.options.get("username") === null) {
+            username = interaction;
+            id = username.user.id;
         } else {
-            nickname = username.member.nickname;
+            username = interaction.options.get("username");
+            id = username.user.id;
         }
-        //get user's avatar
-        var avatar = username.user.avatar;
-        //get user's joined date
-        var joined = new Date(username.member.joinedTimestamp);
-
-        const embed = new EmbedBuilder()
-            .setTitle('User Info - ' + username.user.username)
-            .setColor('#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'))
-            .setThumbnail('https://cdn.discordapp.com/avatars/'+username.value+'/'+avatar+'.png')
-            .setDescription(
-            '\nUsername: ' + tag +
-            '\nNickname: ' + nickname + 
-            '\nDiscord ID: ' + id +
-            '\nJoined Server: ' + joined.toDateString())
-            embed.setFooter({
-                text: "Eggium - Tanner Approved"
-            })
-            .setTimestamp();
-        interaction.reply({ embeds: [embed] });
+        setTimeout(function() {
+            console.log(username)
+            //get user's tag
+            var tag = username.user.tag;
+            //get user's nickname
+            var nickname;
+            if(username.member.nickname === null) {
+                nickname = "[not yet set]"
+            } else {
+                nickname = username.member.nickname;
+            }
+            //get user's avatar
+            var avatar = username.user.avatar;
+            //get user's joined date
+            var joined = new Date(username.member.joinedTimestamp);
+    
+            const embed = new EmbedBuilder()
+                .setTitle('User Info - ' + username.user.username)
+                .setColor('#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'))
+                .setThumbnail('https://cdn.discordapp.com/avatars/'+id+'/'+avatar+'.png')
+                .setDescription(
+                '\nUsername: ' + tag +
+                '\nNickname: ' + nickname + 
+                '\nDiscord ID: ' + id +
+                '\nJoined Server: ' + joined.toDateString())
+                embed.setFooter({
+                    text: "Eggium - Tanner Approved"
+                })
+                .setTimestamp();
+            interaction.reply({ embeds: [embed] });
+        }, 1000);
     }
 };
