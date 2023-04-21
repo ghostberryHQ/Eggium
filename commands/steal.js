ae4fb3337ce45a7a230a69c835a93ef06b77844b
IAmThePerson <34802651+spjoes@users.noreply.github.com>
2023-04-21 12:51:59 -0400

    Updated the emoji /steal command

--- commands/steal.js
+++ commands/steal.js
@@ -12,22 +12,30 @@ module.exports = {
         var emoji = interaction.options.getString("emoji");
         console.log(emoji)
         var arr = emoji.match(/<a:.+?:\d+>|<:.+?:\d+>/g);
-        console.log(arr.length)
         if(arr.length > 1) {
             interaction.reply({content: 'Look man. One emoji at a time. Dont want to be caught by the police now. Do ya?', ephemeral: true});
         } else {
             var emoteIDToSteal = arr[0].match(/\d+/g)[0];
             var emoteNameToSteal = arr[0].match(/(?<=:)[a-zA-Z1-9]+(?=:)/g)[0];
             if(emoji.toString().includes('<a:')) {
-                console.log("gif" + `https://cdn.discordapp.com/emojis/${emoteIDToSteal}.gif?size=44&quality=lossless`);
-                interaction.guild.emojis.create({attachment: `https://cdn.discordapp.com/emojis/${emoteIDToSteal}.gif?size=44&quality=lossless`, name: emoteNameToSteal}).then(emote => {
-                    interaction.reply({ content: `<a:${emote.name}:${emote.id}> | Stole "${emote.name}"` , ephemeral: true});
-                }).catch(console.error);
+                //check if user has permission to add emojis
+                if(interaction.member.permissions.has('MANAGE_EMOJIS_AND_STICKERS')) {
+                    console.log("gif" + `https://cdn.discordapp.com/emojis/${emoteIDToSteal}.gif?size=44&quality=lossless`);
+                    interaction.guild.emojis.create({attachment: `https://cdn.discordapp.com/emojis/${emoteIDToSteal}.gif?size=44&quality=lossless`, name: emoteNameToSteal}).then(emote => {
+                        interaction.reply({ content: `<a:${emote.name}:${emote.id}> | Stole "${emote.name}"` , ephemeral: true});
+                    }).catch(console.error);
+                } else {
+                    interaction.reply({content: 'You do not have permission to add emojis to this server', ephemeral: true});
+                }
             } else {
-                console.log("non-gif " + `https://cdn.discordapp.com/emojis/${emoteIDToSteal}.webp?size=44&quality=lossless`);
-                interaction.guild.emojis.create({attachment: `https://cdn.discordapp.com/emojis/${emoteIDToSteal}.webp?size=44&quality=lossless`, name: emoteNameToSteal}).then(emote => {
-                    interaction.reply({ content: `<:${emote.name}:${emote.id}> | Stole "${emote.name}"` , ephemeral: true});
-                }).catch(console.error);
+                if(interaction.member.permissions.has('MANAGE_EMOJIS_AND_STICKERS')) {
+                    console.log("non-gif " + `https://cdn.discordapp.com/emojis/${emoteIDToSteal}.webp?size=44&quality=lossless`);
+                    interaction.guild.emojis.create({attachment: `https://cdn.discordapp.com/emojis/${emoteIDToSteal}.webp?size=44&quality=lossless`, name: emoteNameToSteal}).then(emote => {
+                        interaction.reply({ content: `<:${emote.name}:${emote.id}> | Stole "${emote.name}"` , ephemeral: true});
+                    }).catch(console.error);
+                } else {
+                    interaction.reply({content: 'You do not have permission to add emojis to this server', ephemeral: true});
+                }
             }
         }
     }
