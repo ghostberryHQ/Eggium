b67a06eae554b8c04c5f3f27e582b3fa9ec2c71e
IAmThePerson <34802651+spjoes@users.noreply.github.com>
2024-06-26 16:26:02 -0400

    Added playlist support!

--- events/distube/addList.ts
+++ events/distube/addList.ts
@@ -3,11 +3,29 @@ import { EmbedBuilder } from "discord.js";
 import { DisTubeEvent, type Metadata } from "../..";
 import type { Playlist, Queue } from "distube";
 import type { ColorResolvable } from "discord.js";
-
+import Song, { type ISong } from '../../models/song.model';
+var mongoUtil = require( '../../utils/mongoUtil' );
+var db = mongoUtil.getDb();
 
 export default class AddListEvent extends DisTubeEvent<Events.ADD_LIST> {
   readonly name = Events.ADD_LIST;
   run(_queue: Queue, playlist: Playlist<Metadata>) {
+
+    //for each song in the playlist, add it to the database
+    playlist.songs.forEach(song => {
+      const songData = new Song({
+        title: song.name!,
+        url: song.url!,
+        status: "queued",
+        server: song.member?.guild.id!,
+        requester: song.member?.user.id!,
+        skipVotes: [],
+      }).save().then(() => {
+        console.log('Song saved to database');
+      });
+    });
+
+
     playlist.metadata.interaction.editReply({
       embeds: [
         new EmbedBuilder()
